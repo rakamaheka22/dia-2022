8c22369124b092529d14d935a26f651a1aaa591b
Raka Maheka Auramuwahid <rakamaheka@gmail.com>
2022-01-31 01:40:57 +0700

    Auth Guard

--- cli/vue-lesson/src/router/index.js
+++ cli/vue-lesson/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import store from '../store';
+
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Login from '../views/Login.vue';
@@ -7,10 +9,10 @@ import Detail from '../views/Detail.vue';
 import Edit from '../views/Edit.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
-  { path: '/login', component: Login },
-  { path: '/:id', component: Detail, props: true, children: [
+  { path: '/', component: Home, meta: { auth: true } },
+  { path: '/about', component: About, meta: { auth: false } },
+  { path: '/login', component: Login, meta: { auth: false } },
+  { path: '/:id', component: Detail, props: true, meta: { auth: true }, children: [
     {
       path: 'edit', component: Edit
     }
@@ -21,6 +23,22 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+
+  if (to.matched.some(record => record.meta.auth)) {
+    
+    const isLoggedIn = await store.dispatch('isUserLoggon');
+
+    if (!isLoggedIn) {
+      next('/login');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
